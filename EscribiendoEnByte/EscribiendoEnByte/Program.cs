8e66b610e6dd6f2035a991c6fbd8a0d7aebb84f9
RicardoGmPr <70128085+RicardoGmPr@users.noreply.github.com>
2022-06-06 23:01:59 -0700

    Add files via upload

---
+++ EscribiendoEnByte/EscribiendoEnByte/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EscribiendoEnByte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = null;
+            byte[] buffer = new byte[81];
+            int nbytes = 0, car;
+            try
+            {
+                // Crear el flujo del archivo
+                fs = new FileStream("text.txt", FileMode.Create, FileAccess.Write);
+                Console.WriteLine("Escriba el texto que desee almacenar en el archivo");
+                while ((car = Console.Read()) != '\r' && (nbytes < buffer.Length))
+                {
+                    buffer[nbytes] = (byte)car;
+                    nbytes++;
+                }
+                fs.Write(buffer, 0, nbytes);
+            }
+            catch (IOException e)
+            {
+                Console.Error.WriteLine(e.Message);
+            }
+
+            finally
+            {
+                if (fs != null) fs.Close();
+            }
+        }
+    }
+}
